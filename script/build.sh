22b7908be97c9c2e3f8dd531a7c1584135ef7a1f
Valery Mironov <valera.mironow@gmail.com>
2022-03-26 06:55:08 +0300

    Add choose YACLib branch Add compare.sh script

--- script/build.sh
+++ script/build.sh
@@ -4,19 +4,19 @@ cmake -S . -B ./build_clang_libcxx \
   -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
-  -DLIBCXX=ON -DSTD=ON -DYACLIB=ON
+  -DLIBCXX=ON -DSTD=ON -DYACLIB=main
 
 cmake -S . -B ./build_clang_libstdcxx \
   -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
-  -DSTD=ON -DYACLIB=ON -DFOLLY=ON -DBOOST_THREAD=ON
+  -DSTD=ON -DYACLIB=main -DFOLLY=ON -DBOOST_THREAD=ON
 
 cmake -S . -B ./build_gcc_libstdcxx \
   -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
-  -DSTD=ON -DYACLIB=ON -DFOLLY=ON -DBOOST_THREAD=ON
+  -DSTD=ON -DYACLIB=main -DFOLLY=ON -DBOOST_THREAD=ON
 
 ninja -C ./build_clang_libcxx
 
